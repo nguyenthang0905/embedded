0329de92f21fe957f1c4c2c636b899be89de47e2
Nguyễn Văn Thắng (1M-Consumer-KSP-MB) <thangnv12@onemount.com>
2021-06-15 23:22:47 +0700

    Login

--- web/_callback.php
+++ web/_callback.php
@@ -6,6 +6,19 @@
     <meta name="salesforce-mode" content="inline-callback">
     <meta name="salesforce-save-access-token" content="true">
     <meta name="salesforce-logout-on-browser-close" content="true">
+
+    <script src="https://devsb-ditest001b2bcommercelex.cs73.force.com/b2bcomlex/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
+
+    <script>
+        function onlogin(identity) {
+            console.log('CALLBACK');
+            console.log(identity);
+        }
+        function onLogout() {
+            console.log('LOGOUT');
+            SFIDWidget.init();
+        }
+    </script>
 </head>
 <body>
     
