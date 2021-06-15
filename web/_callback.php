6557d1a86af3f9b0b0dc4a85132ae6f22910d14a
Nguyễn Văn Thắng (1M-Consumer-KSP-MB) <thangnv12@onemount.com>
2021-06-15 22:35:50 +0700

    Login

---
+++ web/_callback.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="salesforce-community" content="https://devsb-ditest001b2bcommercelex.cs73.force.com/b2bcomlex">
+    <meta name="salesforce-allowed-domains" content="https://embedded-login1.herokuapp.com">
+    <meta name="salesforce-mode" content="inline-callback">
+    <meta name="salesforce-save-access-token" content="true">
+    <meta name="salesforce-logout-on-browser-close" content="true">
+</head>
+<body>
+    
+</body>
+</html>
