5eb42308c75ecdad529ec227db0ff28611ccdce8
Nguyễn Văn Thắng (1M-Consumer-KSP-MB) <thangnv12@onemount.com>
2021-06-16 15:17:17 +0700

    Change community

--- web/_callback.php
+++ web/_callback.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta name="salesforce-community" content="https://devsb-ditest001b2bcommercelex.cs73.force.com/b2bcomlex">
+    <meta name="salesforce-community" content="https://devsb-ditest001b2bcommercelex.cs73.force.com/b2bCommLEX03">
     <meta name="salesforce-allowed-domains" content="embedded-login1.herokuapp.com">
     <meta name="salesforce-mode" content="inline-callback">
     <meta name="salesforce-save-access-token" content="true">
     <meta name="salesforce-logout-on-browser-close" content="true">
 
-    <script src="https://devsb-ditest001b2bcommercelex.cs73.force.com/b2bcomlex/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
+    <script src="https://devsb-ditest001b2bcommercelex.cs73.force.com/b2bCommLEX03/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
 </head>
 <body>
     
